e70cd5f6a322340cbb8c3befe925aa3544ebb507
kdr250 <54400975+kdr250@users.noreply.github.com>
2026-01-16 16:04:07 +0900

    display window

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,33 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!" << std::endl;
+    // initialize glfw
+    if (glfwInit() == GLFW_FALSE)
+    {
+        return EXIT_FAILURE;
+    }
+
+    // create window
+    GLFWwindow* window = glfwCreateWindow(640, 480, "WebGPU Ocean", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    glfwSwapInterval(1);
+
+    // game loop
+    while (glfwWindowShouldClose(window) == GLFW_FALSE)
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // terminate glfw
+    glfwTerminate();
 
     return 0;
 }
